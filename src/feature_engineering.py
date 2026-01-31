3112155a2f07c237e26d6719903e2d10a1fb64c3
orbimatrix <statducks99@gmail.com>
2026-02-01 03:18:43 +0500

    fixed skewness via quantile transformation

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,6 +1,7 @@
 # feature_engineering.py
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import QuantileTransformer
 
 class FeatureEngineer:
     def __init__(self, train_path, test_path):
@@ -25,6 +26,16 @@ class FeatureEngineer:
         df['TAX_PER_RM'] = df['TAX'] / (df['RM'] + 1)
         return df
 
+    def fix_stubborn_skewness(self, df):
+        # QuantileTransformer forces the data into a normal distribution
+        # This is great for 'CRIM' and 'B' which are extremely non-normal in our dataset
+        qt = QuantileTransformer(output_distribution='normal', random_state=42)
+        
+        cols_to_fix = ['CRIM', 'ZN', 'B']
+        df[cols_to_fix] = qt.fit_transform(df[cols_to_fix])
+    
+        return df
+
     def initiate_feature_engineering(self):
         print("Starting Feature Engineering...")
         train_df = pd.read_csv(self.train_path)
@@ -32,12 +43,12 @@ class FeatureEngineer:
 
         # 1. Fix Skewness
         skewed_features = ['CRIM', 'ZN', 'LSTAT', 'MEDV','B']
-        train_df = self.apply_log_transform(train_df, skewed_features)
-        test_df = self.apply_log_transform(test_df, skewed_features)
+        # train_df = self.apply_log_transform(train_df, skewed_features)
+        # test_df = self.apply_log_transform(test_df, skewed_features)
 
-        # 2. Add custom logic
-        train_df = self.create_new_features(train_df)
-        test_df = self.create_new_features(test_df)
+        # # 2. Added custom logic
+        # train_df = self.create_new_features(train_df)
+        # test_df = self.create_new_features(test_df)
 
         # Save the engineered data
         train_df.to_csv("data/processed/train_engineered.csv", index=False)
@@ -47,5 +58,10 @@ class FeatureEngineer:
         return "data/processed/train_engineered.csv", "data/processed/test_engineered.csv"
 
 if __name__ == "__main__":
-    engineer = FeatureEngineer("data/processed/train_processed.csv", "data/processed/test_processed.csv")
+    engineer = FeatureEngineer("data/processed/train_engineered.csv", "data/processed/test_engineered.csv")
     engineer.initiate_feature_engineering()
+
+
+# While Log Transformation normalized price and status, features like Crime and ZN remained heavily zero-inflated. 
+# I implemented a QuantileTransformer to map these features to a Gaussian distribution, ensuring the regression
+#  model's assumptions were met for these extreme variables
